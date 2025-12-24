4ab77ed426dd1672d077cd145882642b6e419948
NEM3S <mathis39410@gmail.com>
2025-12-24 02:28:23 +0100

    fix: forgot console color reset

--- VALORez/Model/Utils/ConsoleWriter.cs
+++ VALORez/Model/Utils/ConsoleWriter.cs
@@ -4,21 +4,21 @@ public abstract class ConsoleWriter
 {
     public static void PrintSuccess(string message)
     {
-        Console.WriteLine($" [\x1B[32m\u2713\x1B[97m][{DateTime.Now:T}] - {message}.");
+        Console.WriteLine($" [\x1B[32m\u2713\x1B[0m][{DateTime.Now:T}] - {message}.");
     }
     
     public static void PrintFailure(string message)
     {
-        Console.WriteLine($" [\x1B[31mx\x1B[97m][{DateTime.Now:T}] - {message}!");
+        Console.WriteLine($" [\x1B[31mx\x1B[0m][{DateTime.Now:T}] - {message}!");
     }
     
     public static void PrintInfo(string message)
     {
-        Console.WriteLine($" [\x1B[33mO\x1B[97m][{DateTime.Now:T}] - {message}.");
+        Console.WriteLine($" [\x1B[33mO\x1B[0m][{DateTime.Now:T}] - {message}.");
     }
     
     public static void PrintError(string message)
     {
-        Console.WriteLine($" [\x1B[35mERROR\x1B[97m][{DateTime.Now:T}] - {message}");
+        Console.WriteLine($" [\x1B[35mERROR\x1B[0m][{DateTime.Now:T}] - {message}");
     }
 }
